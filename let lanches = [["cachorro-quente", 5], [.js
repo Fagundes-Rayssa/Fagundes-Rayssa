a4c584e756385829d035e21a22b85636be937537
Fagundes-Rayssa <164500870+Fagundes-Rayssa@users.noreply.github.com>
2025-03-17 12:15:20 -0300

    Add files via upload

---
+++ let lanches = [["cachorro-quente", 5], [.js
@@ -0,0 +1,37 @@
+let lanches = [["cachorro-quente", 5], ["paçoca", 2], ["rifregerante-suco", 4], ["Pastel", 8], ["pipoca", 5]];
+
+
+function mostraCardapio() {
+   console.log("Cardápio:");
+   lanches.forEach((item, index) => {
+       console.log(`${index + 1} - ${item[0]} R$ ${item[1]}`);
+   });
+}
+
+
+
+
+function adicionaLanche(nome, preco) {
+   lanches.push([nome, valor]);
+   console.log(`${nome} foi adicionado ao cardápio com o valor de R$ ${valor}`);
+}
+
+
+
+
+function cardapioEspecial() {
+   console.log("\nCardápio Especial para Clientes Fidelizados (15% de Desconto):");
+   lanches.forEach((item, index) => {
+       let precoDesconto = item[1] * 0.85;
+       console.log(`${index + 1} - ${item[0]} R$ ${precoDesconto.toFixed(2)}`);
+   });
+}
+
+
+
+
+console.log("Executando o Cardápio...");
+mostraCardapio();
+adicionaLanche("coxinha", 6);
+mostraCardapio();
+cardapioEspecial();
